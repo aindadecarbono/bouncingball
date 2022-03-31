2e4299eea508d1ff618c3a9df9fed7c590af864c
bruno <92956944+aindadecarbono@users.noreply.github.com>
2022-03-31 13:06:12 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,26 @@
 var y = 0;
-var velY = 8;
+var velY = 12;
+var x = 0;
+var velX = 12;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	
-}
+	createCanvas(200, 200);
+	}
 
 function draw() {
-	background(255);
-	ellipse(windowWidth/2, y, 20, 20);
+	background('rgba(54, 29, 46, 0.45)');
+	noStroke();
+	fill('rgba(0,255,0, 0.8)');
+	
+	ellipse(width/2, y, 20, 20);
 	y = y + velY;
 	if (y > height || y < 1){
 		velY *= -1;
+	}
+	
+	ellipse(x, height/2, 20, 20);
+	x = x + velX;
+	if (x > width || x < 1){
+		velX *= -1;
 	}
 }
